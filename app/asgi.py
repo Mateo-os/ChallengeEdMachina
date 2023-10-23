674e085debe4702dae5b773bfd2c211ab25f9a94
Mateo Ostorero <mateoostorero71@gmail.com>
2023-10-23 17:45:46 -0300

    added a redirect to docs

--- app/asgi.py
+++ app/asgi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from database import db
 from main.controller import lead_controller, course_controller
 
@@ -11,9 +12,14 @@ def create_app():
     )
 
     db.init_app(app)
+
     app.include_router(lead_controller, prefix="/leads", tags=["leads"])
 
     app.include_router(course_controller, prefix="/courses", tags=["courses"])
+
+    @app.get("/", include_in_schema=False)
+    async def docs_redirect():
+        return RedirectResponse(url="/docs")
 
     return app
 
